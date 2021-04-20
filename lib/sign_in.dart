b8c534d38b8bf53478765b4df6bd7e5acebc468e
chafidhoturrochimah <chafidhotur96@gmail.com>
2021-04-20 17:01:32 +0700

    update hasil praktikum

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -4,14 +4,17 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
+
 String name;
 String email;
 String imageUrl;
+
 Future<String> signInWithGoogle() async {
   await Firebase.initializeApp();
+
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
   final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+    await googleSignInAccount.authentication;
   final AuthCredential credential = GoogleAuthProvider.credential(
     accessToken: googleSignInAuthentication.accessToken,
     idToken: googleSignInAuthentication.idToken,
@@ -19,6 +22,7 @@ Future<String> signInWithGoogle() async {
   final UserCredential authResult =
       await _auth.signInWithCredential(credential);
   final User user = authResult.user;
+  
   if (user != null) {
     // Checking if email and name is null
     assert(user.email != null);
